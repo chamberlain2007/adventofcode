ecb7775041afef3a93414d6f3af1a48d4d6f9dd3
Matthew FitzGerald-Chamberlain <chamberlain2007@gmail.com>
2019-12-07 20:58:56 -0600

    Cleanup linting errors in day 2

--- day2part1.js
+++ day2part1.js
@@ -18,27 +18,27 @@ const handleOperation = (array, index, operation) => {
  * Generate the add function
  * @param {number[]} array The array of instructions
  * @param {number} index The index to add
- * @returns {function(number,number): number} The add function
+ * @return {function(number,number): number} The add function
  */
-const add = (array, index) => handleOperation(array, index, (operand1, operand2) => operand1 + operand2);
+const add = (array, index) => handleOperation(array, index, (operand1, operand2) => operand1 + operand2); // eslint-disable-line max-len
 
 /**
  * Generate the multiply function
  * @param {number[]} array The array of instructions
  * @param {number} index The index to multiply
- * @returns {function(number,number): number} The multiply function
+ * @return {function(number,number): number} The multiply function
  */
-const multiply = (array, index) => handleOperation(array, index, (operand1, operand2) => operand1 * operand2);
+const multiply = (array, index) => handleOperation(array, index, (operand1, operand2) => operand1 * operand2); // eslint-disable-line max-len
 
 /**
  * Parse the opCode at a given index
  * @param {number[]} array The array of instructions
  * @param {number} index The index to parse
- * @returns {boolean} Whether the computer should stop
+ * @return {boolean} Whether the computer should stop
  */
 const parseOpCode = (array, index) => {
     const opCode = array[index];
-    switch(opCode) {
+    switch (opCode) {
         case 1:
             add(array, index);
             return false;
@@ -55,9 +55,9 @@ const parseOpCode = (array, index) => {
 /**
  * Parse the array of instructions
  * @param {number[]} array The array of instructions
- * @returns {number[]} The computed value based on the instructions in the array
+ * @return {number[]} The computed value based on the instructions in the array
  */
-const parseArray = array => {
+const parseArray = (array) => {
     const result = [...array];
     let index = 0;
     while (index < result.length) {
@@ -67,18 +67,23 @@ const parseArray = array => {
         index += 4;
     }
     return result;
-}
+};
 
 module.exports = parseArray;
 
 if (require.main === module) {
-    let testArray = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0];
+    const fs = require('fs');
+
+    const input = fs.readFileSync('day2input.txt', 'utf8');
+
+    const testArray = [...input
+        .split(',')
+        .map((val) => parseInt(val))
+        .filter((val) => !isNaN(val))];
 
     testArray[1] = 12;
     testArray[2] = 2;
-    
-    let resultArray = parseArray(testArray);
-    
+    const resultArray = parseArray(testArray);
+
     console.log(resultArray[0]);
 }
-
--- day2part2.js
+++ day2part2.js
@@ -1,19 +1,26 @@
 const parseArray = require('./day2part1');
 
 if (require.main === module) {
-    let testArray = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0];
+    const fs = require('fs');
+
+    const input = fs.readFileSync('day1input.txt', 'utf8');
+
+    const testArray = [...input
+        .split(',')
+        .map((val) => parseInt(val))
+        .filter((val) => !isNaN(val))];
 
     const maxAttempt = 100;
     const targetValue = 19690720;
-    
+
     for (let i = 1; i < maxAttempt; i++) {
         for (let j = 1; j < maxAttempt; j++) {
-            let testArrayClone = [...testArray];
+            const testArrayClone = [...testArray];
             testArrayClone[1] = i;
             testArrayClone[2] = j;
-            let resultArray = parseArray(testArrayClone);
-            let result = resultArray[0];
-    
+            const resultArray = parseArray(testArrayClone);
+            const result = resultArray[0];
+
             if (result === targetValue) {
                 console.log(100 * i + j);
                 return;

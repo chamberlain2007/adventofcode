03b00ca1393900f768c41f2a747da40d0f2493e5
Matthew FitzGerald-Chamberlain <chamberlain2007@gmail.com>
2019-12-07 20:45:47 -0600

    Cleanup linting errors in day 1 part 2

--- day1part2.js
+++ day1part2.js
@@ -1,18 +1,20 @@
 /**
  * Calculate the fuel for a given fuel quantity
  * @param {number} fuel The amount of fuel to calculate the fuel for
+ * @return {number} The fuel needed for the fuel
  */
-const calculateFuelFuel = fuel => Math.floor(fuel / 3.0) - 2;
+const calculateFuelFuel = (fuel) => Math.floor(fuel / 3.0) - 2;
 
 /**
  * Calculate the fuel for a given mass
  * @param {number} mass The mass to calculate the fuel for
+ * @return {number} The fuel needed for the mass
  */
-const calculateFuel = mass => {
+const calculateFuel = (mass) => {
     const massFuel = Math.floor(mass / 3.0) - 2;
     let currentFuel = massFuel;
     let currentFuelFuel = calculateFuelFuel(massFuel);
-    while(currentFuelFuel > 0) {
+    while (currentFuelFuel > 0) {
         currentFuel += currentFuelFuel;
         currentFuelFuel = calculateFuelFuel(currentFuelFuel);
     }
@@ -26,7 +28,12 @@ if (require.main === module) {
 
     const input = fs.readFileSync('day1input.txt', 'utf8');
 
-    const sum = input.split('\n').map((val) => parseInt(val)).filter((val) => !!val).map(calculateFuel).reduce((prev, cur) => prev + cur, 0);
-    
+    const sum = input
+        .split('\n')
+        .map((val) => parseInt(val))
+        .filter((val) => !!val)
+        .map(calculateFuel)
+        .reduce((prev, cur) => prev + cur, 0);
+
     console.log(sum);
 }

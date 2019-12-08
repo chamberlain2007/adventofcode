97e17f612fad2bf422b2187ab64493d6e4216f90
Matthew FitzGerald-Chamberlain <chamberlain2007@gmail.com>
2019-12-07 21:40:33 -0600

    Cleanup linting errors in day 4

--- day4part1.js
+++ day4part1.js
@@ -1,39 +1,66 @@
-const validateAscendingDigits = candidate => {
+/**
+ * Validates whether all of the digits are greater than the previous
+ * @param {number} number The number to check
+ * @return {boolean} True if all of the digits are valid
+ */
+const validateAscendingDigits = (number) => {
     let previous = -1;
     let isValid = true;
 
-    candidate.toString().split('').map(x => parseInt(x)).forEach(current => {
+    number.toString().split('').map((x) => parseInt(x)).forEach((current) => {
         if (current < previous) {
             isValid = false;
         }
         previous = current;
     });
-    
+
     return isValid;
 };
 
-const validateHasAdjacentDigits = candidate => {
+/**
+ * Validate whether there is at least one set of duplicate digits
+ * @param {number} number The number to check
+ * @return {boolean} True if there is a set of duplicate digits
+ */
+const validateHasAdjacentDigits = (number) => {
     let previous = -1;
     let isValid = false;
 
-    candidate.toString().split('').map(x => parseInt(x)).forEach(current => {
+    number.toString().split('').map((x) => parseInt(x)).forEach((current) => {
         if (current === previous) {
             isValid = true;
         }
         previous = current;
     });
-    
+
     return isValid;
 };
 
-const validateRange = number => number >= 100000 && number <= 999999;
+/**
+ * Validates that the number is 6 digits long
+ * @param {number} number The number to validate
+ * @return {boolean} True if the number is 6 digits long
+ */
+const validateRange = (number) => number >= 100000 && number <= 999999;
 
-let validCount = 0;
+/**
+ * Validates a number
+ * @param {number} number The number to validate
+ * @return {boolean} True if the number is valid
+ */
+const validateNumber = (number) => validateRange(number) && validateAscendingDigits(number) && validateHasAdjacentDigits(number); // eslint-disable-line max-len
 
-for (let i = 156218; i <= 652527; i++) {
-    if (validateRange(i) && validateAscendingDigits(i) && validateHasAdjacentDigits(i)) {
-        validCount++;
-    }
+module.exports = validateNumber;
+
+if (require.main === module) {
+    const min = 156218;
+    const max = 652527;
+
+    const validCount = [...Array(max-min).keys()]
+        .map((val) => val + min)
+        .reduce((cur, val) => {
+            return cur + validateNumber(val);
+        }, 0);
+
+    console.log(validCount);
 }
-
-console.log(validCount);

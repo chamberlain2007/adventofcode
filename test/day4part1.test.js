efa403dab6d0efccae6070c231294de44914f19a
Matthew FitzGerald-Chamberlain <chamberlain2007@gmail.com>
2019-12-07 21:44:36 -0600

    Add tests for day 4

---
+++ test/day4part1.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const day4part1 = require('../day4part1');
+
+describe('Day 4 Part 1', () => {
+    it('Should return true for 111111', () => {
+        let valid = day4part1(111111);
+        assert.equal(valid, true);
+    });
+
+    it('Should return false for 223450', () => {
+        let valid = day4part1(223450);
+        assert.equal(valid, false);
+    });
+
+    it('Should return false for 123789', () => {
+        let valid = day4part1(123789);
+        assert.equal(valid, false);
+    });
+})
---
+++ test/day4part2.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const day4part2 = require('../day4part2');
+
+describe('Day 4 Part 2', () => {
+    it('Should return true for 112233', () => {
+        let valid = day4part2(112233);
+        assert.equal(valid, true);
+    });
+
+    it('Should return false for 123444', () => {
+        let valid = day4part2(123444);
+        assert.equal(valid, false);
+    });
+
+    it('Should return false for 111122', () => {
+        let valid = day4part2(111122);
+        assert.equal(valid, true);
+    });
+})

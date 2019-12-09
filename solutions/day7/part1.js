101413ab9ce761bff7a3c144aaa05c442e7e3f45
Matthew FitzGerald-Chamberlain <chamberlain2007@gmail.com>
2019-12-08 21:51:45 -0600

    Refactor computer to a common file and fix linting errors in day 7 part 1

--- solutions/day7/part1.js
+++ solutions/day7/part1.js
@@ -1,163 +1,45 @@
-const readlineSync = require('readline-sync');
+const Computer = require('./computer');
 const generatePermutations = require('../../utils/generate-permutations');
 
-const Computer = class {
-    constructor(stdin, stdout, instructionList) {
-        this.stdin = stdin;
-        this.stdout = stdout;
-        this.instructionList = instructionList;
-
-        this.operations = {
-            1: [this.add(), 4],
-            2: [this.multiply(), 4],
-            3: [this.read(), 2],
-            4: [this.save(), 2],
-            5: [this.jumpIfTrue()],
-            6: [this.jumpIfFalse()],
-            7: [this.lessThan(), 4],
-            8: [this.equals(), 4],
-            99: []
-        };
-    }
-
-    getOperands(startIndex, immediateModeSettings, parameterCount) {
-        const operands = [];
-
-        for (let i = 0; i < parameterCount; i++) {
-            let operand = 0;
-            let immediateMode = !!immediateModeSettings[i];
-            if (immediateMode) {
-                operand = this.activeInstructionList[startIndex+i+1];
-            }
-            else {
-                let location = this.activeInstructionList[startIndex+i+1];
-                operand = this.activeInstructionList[location];
-            }
-            operands.push(operand);
-        }
-        return operands;
-    }
-
-    jumpOperation(operation) {
-        return (startIndex, immediateModeSettings) => {
-            const operands = this.getOperands(startIndex, immediateModeSettings, operation.length);
-            const newIndex = operation.apply(this, operands);
-            if (newIndex === undefined) {
-                return operation.length+1;
-            }
-            else {
-                return newIndex - startIndex;
-            }
-        }
-    }
-    
-    normalOperation(operation) {
-        return (startIndex, immediateModeSettings) => {
-            const operands = this.getOperands(startIndex, immediateModeSettings, operation.length);
-            const result = operation.apply(this, operands);
-            if (result === undefined) {
-                return operation.length+1;
-            }
-            const resultIndex = this.activeInstructionList[startIndex+operation.length+1];
-            this.activeInstructionList[resultIndex] = result;
-            return operation.length+1;
-        }
-    }
-    
-    add() {
-        return this.normalOperation((operand1, operand2) => operand1 + operand2);
-    }
-    
-    multiply() {
-        return this.normalOperation((operand1, operand2) => operand1 * operand2);
-    }
-    
-    read() {
-        return this.normalOperation(() => this.stdin());
-    }
-    
-    save() {
-        return this.normalOperation(operand1 => this.stdout(operand1));
-    }
-    
-    lessThan() {
-        return this.normalOperation((operand1, operand2) => operand1 < operand2 ? 1 : 0);
-    }
-    
-    equals() {
-        return this.normalOperation((operand1, operand2) => operand1 === operand2 ? 1 : 0);
-    }
-    
-    jumpIfTrue() {
-        return this.jumpOperation((operand1, operand2) => operand1 !== 0 ? operand2 : undefined);
-    }
-    
-    jumpIfFalse() {
-        return this.jumpOperation((operand1, operand2) => operand1 === 0 ? operand2 : undefined);
-    }
-
-    parseOpCode(instruction) {
-        if (instruction < 100) {
-            return [instruction, []];
-        }
-        const instructionStr = instruction.toString();
-        const opCode = parseInt(instructionStr.slice(-2));
-        const immediateModeSettings = instructionStr.substr(0, instructionStr.length - 2).split('').reverse().map(x => x === '1');
-        return [opCode, immediateModeSettings];
-    }
-
-    parseInstruction(index) {
-        const [opCode, immediateModeSettings] = this.parseOpCode(this.activeInstructionList[index]);
-        const instruction = this.operations[opCode] || [];
-        const [operation, skip] = instruction;
-    
-        if (!operation) {
-            return [true, 1];
-        }
-    
-        const result = operation(index, immediateModeSettings);
-    
-        return [false, skip === undefined ? result : skip];
-    }
-
-    run() {
-        this.activeInstructionList = [...this.instructionList];
-        let index = 0;
-        while (index < this.activeInstructionList.length) {
-            let [shouldBreak, step] = this.parseInstruction(index);
-            if (shouldBreak) {
-                break;
-            }
-            index += step;
-        }
-        return this.activeInstructionList;
-    }
-};
-
+/**
+ * Represents the amplifier circuit in which there are 5 computers where
+ * the output of the first 4 are connected, and the last is the thrust output
+ * @param {number[]} phaseSettings The initial values to pass to the computers
+ * @param {number[]} instructionList The instruction list
+ */
 const AmplifierCircuit = class {
+    /**
+     * Default constructor
+     * @param {number[]} phaseSettings The initial values to pass to the computers
+     * @param {number[]} instructionList The instruction list
+     */
     constructor(phaseSettings, instructionList) {
         this.phaseSettings = phaseSettings;
         this.instructionList = instructionList;
     }
 
+    /**
+     * Run the amplifier circuit
+     * @param {number} input The initial value to pass to the first computer
+     * @return {number} The thrust output value
+     */
     run(input) {
         let currentValue = input;
         for (let i = 0; i < this.phaseSettings.length; i++) {
-            let phaseSetting = this.phaseSettings[i];
+            const phaseSetting = this.phaseSettings[i];
             let inputCount = 0;
-            let computer = new Computer(
+            const computer = new Computer(
                 () => {
                     if (inputCount++ === 0) {
                         return phaseSetting;
-                    }
-                    else {
+                    } else {
                         return currentValue;
                     }
                 },
-                val => {
+                (val) => {
                     currentValue = val;
                 },
-                this.instructionList
+                this.instructionList,
             );
             computer.run();
         }
@@ -171,9 +53,9 @@ const instructionList = readFileToArray('amplifiercircuitinput.txt', ',');
 let maxThrustOutput = 0;
 let bestPhaseSettings = [];
 
-generatePermutations([0,1,2,3,4]).forEach(phaseSettings => {
-    let amplifierCircuit = new AmplifierCircuit(phaseSettings, instructionList);
-    let thrustOutput = amplifierCircuit.run(0);
+generatePermutations([0, 1, 2, 3, 4]).forEach((phaseSettings)=> {
+    const amplifierCircuit = new AmplifierCircuit(phaseSettings, instructionList);
+    const thrustOutput = amplifierCircuit.run(0);
 
     if (thrustOutput > maxThrustOutput) {
         maxThrustOutput = thrustOutput;

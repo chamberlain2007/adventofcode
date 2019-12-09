1faec2c6bc8707bcaf196e843264bc0ceddcd930
Matthew FitzGerald-Chamberlain <chamberlain2007@gmail.com>
2019-12-08 21:46:01 -0600

    Cleanup linting errors in day 7 part 2

--- solutions/day7/part2.js
+++ solutions/day7/part2.js
@@ -6,6 +6,11 @@ const generatePermutations = require('../../utils/generate-permutations');
  * @param {number[]} immediateModeSettings The immediate mode settings
  */
 const OpCodeParsingResult = class {
+    /**
+     * Default constructor
+     * @param {number} opCode The opCode
+     * @param {number[]} immediateModeSettings The immediate mode settings
+     */
     constructor(opCode, immediateModeSettings) {
         this.opCode = opCode;
         this.immediateModeSettings = immediateModeSettings;
@@ -59,7 +64,7 @@ const Computer = class {
             6: [this.jumpIfFalse()],
             7: [this.lessThan(), 4],
             8: [this.equals(), 4],
-            99: []
+            99: [],
         };
     }
 
@@ -68,19 +73,18 @@ const Computer = class {
      * @param {number} startIndex The index of the instruction to retrieve the operands for
      * @param {number[]} immediateModeSettings The immediate mode settings for the instruction
      * @param {number} parameterCount The number of parameters for the instruction
-     * @returns {number[]} The operands in the instruction
+     * @return {number[]} The operands in the instruction
      */
     getOperands(startIndex, immediateModeSettings, parameterCount) {
         const operands = [];
 
         for (let i = 0; i < parameterCount; i++) {
             let operand = 0;
-            let immediateMode = !!immediateModeSettings[i];
+            const immediateMode = !!immediateModeSettings[i];
             if (immediateMode) {
                 operand = this.activeInstructionList[startIndex+i+1];
-            }
-            else {
-                let location = this.activeInstructionList[startIndex+i+1];
+            } else {
+                const location = this.activeInstructionList[startIndex+i+1];
                 operand = this.activeInstructionList[location];
             }
             operands.push(operand);
@@ -91,7 +95,7 @@ const Computer = class {
     /**
      * Generate a function for a jump operation.
      * @param {function(...number): number} operation The operation to execute which returns the number of steps to move.
-     * @returns {function(number, number[]): number} The number of steps to jump
+     * @return {function(number, number[]): number} The number of steps to jump
      */
     jumpOperation(operation) {
         return (startIndex, immediateModeSettings) => {
@@ -99,17 +103,16 @@ const Computer = class {
             const newIndex = operation.apply(this, operands);
             if (newIndex === undefined) {
                 return operation.length+1;
-            }
-            else {
+            } else {
                 return newIndex - startIndex;
             }
-        }
+        };
     }
-    
+
     /**
      * Generate a normal operation which returns a value.
      * @param {function(...number): number} operation The operation to execute which returns a value
-     * @returns {function(number, number[]): number} The returned value of the operation
+     * @return {function(number, number[]): number} The returned value of the operation
      */
     normalOperation(operation) {
         return (startIndex, immediateModeSettings) => {
@@ -121,60 +124,68 @@ const Computer = class {
             const resultIndex = this.activeInstructionList[startIndex+operation.length+1];
             this.activeInstructionList[resultIndex] = result;
             return operation.length+1;
-        }
+        };
     }
-    
+
     /**
      * Add two operands
+     * @return {function(...number): number} The add function
      */
     add() {
         return this.normalOperation((operand1, operand2) => operand1 + operand2);
     }
-    
+
     /**
      * Multiply two operands
+     * @return {function(...number): number} The multiply function
      */
     multiply() {
         return this.normalOperation((operand1, operand2) => operand1 * operand2);
     }
-    
+
     /**
      * Read a value from stdin
+     * @return {function(...number): number} The read function
      */
     read() {
         return this.normalOperation(() => this.stdin());
     }
-    
+
     /**
      * Write a value to stdout
+     * @return {function(...number): number} The save function
      */
     save() {
-        return this.normalOperation(operand1 => this.stdout(operand1));
+        return this.normalOperation((operand1) => this.stdout(operand1));
     }
-    
+
     /**
      * Compare two operands, returning 1 if the first is less than the second, otherwise 0
+     * @return {function(...number): number} The less than function
      */
     lessThan() {
         return this.normalOperation((operand1, operand2) => operand1 < operand2 ? 1 : 0);
     }
-    
+
     /**
      * Compare two operands, returning 1 if the two are equal, otherwise 0
+     * @return {function(...number): number} The equals function
      */
     equals() {
         return this.normalOperation((operand1, operand2) => operand1 === operand2 ? 1 : 0);
     }
-    
+
     /**
      * If the first operand isn't 0, jump by the second operand
+     * @return {function(...number): number} The jump function
      */
     jumpIfTrue() {
         return this.jumpOperation((operand1, operand2) => operand1 !== 0 ? operand2 : undefined);
     }
-    
+
     /**
      * If the first operand is 0, jump by the second operand
+     * @return {function(...number): number} The jump function
      */
     jumpIfFalse() {
         return this.jumpOperation((operand1, operand2) => operand1 === 0 ? operand2 : undefined);
@@ -183,7 +194,7 @@ const Computer = class {
     /**
      * Parse an instruction into its opCode and list of immediate mode settings
      * @param {number} instruction The instruction to parse
-     * @returns {OpCodeParsingResult} The opCode and list of immediate mode settings
+     * @return {OpCodeParsingResult} The opCode and list of immediate mode settings
      */
     parseOpCode(instruction) {
         if (instruction < 100) {
@@ -191,26 +202,26 @@ const Computer = class {
         }
         const instructionStr = instruction.toString();
         const opCode = parseInt(instructionStr.slice(-2));
-        const immediateModeSettings = instructionStr.substr(0, instructionStr.length - 2).split('').reverse().map(x => x === '1');
+        const immediateModeSettings = instructionStr.substr(0, instructionStr.length - 2).split('').reverse().map((x) => x === '1');
         return new OpCodeParsingResult(opCode, immediateModeSettings);
     }
 
     /**
      * Parse the instruction in the instruction list at the given index
      * @param {number} index The instruction index
-     * @returns {InstructionParsingResult} The results of the parsing
+     * @return {InstructionParsingResult} The results of the parsing
      */
     parseInstruction(index) {
         const parsedOpCode = this.parseOpCode(this.activeInstructionList[index]);
         const instruction = this.operations[parsedOpCode.opCode] || [];
         const [operation, skip] = instruction;
-    
+
         if (!operation) {
             return new InstructionParsingResult(true, 1);
         }
-    
+
         const result = operation(index, parsedOpCode.immediateModeSettings);
-    
+
         return new InstructionParsingResult(false, skip === undefined ? result : skip);
     }
 
@@ -224,7 +235,7 @@ const Computer = class {
             if (!this.running) {
                 break;
             }
-            let parseInstructionResult = this.parseInstruction(this.index);
+            const parseInstructionResult = this.parseInstruction(this.index);
             if (parseInstructionResult.shouldBreak) {
                 break;
             }
@@ -269,27 +280,26 @@ const AmplifierCircuit = class {
     /**
      * Run the amplifier circuit
      * @param {number} input The initial value to pass to the first computer
-     * @returns {number} The thrust output value
+     * @return {number} The thrust output value
      */
     run(input) {
         let currentValue = input;
-        const computers = this.phaseSettings.map(phaseSetting => {
+        const computers = this.phaseSettings.map((phaseSetting) => {
             let hasUsedPhaseSetting = false;
             const computer = new Computer(
                 () => {
                     if (!hasUsedPhaseSetting) {
                         hasUsedPhaseSetting = true;
                         return phaseSetting;
-                    }
-                    else {
+                    } else {
                         return currentValue;
                     }
                 },
-                val => {
+                (val) => {
                     computer.pause();
                     currentValue = val;
                 },
-                this.instructionList
+                this.instructionList,
             );
             return computer;
         });
@@ -298,11 +308,10 @@ const AmplifierCircuit = class {
 
         while (true) {
             const previousCurrentValue = currentValue;
-            computers.forEach(computer => {
+            computers.forEach((computer) => {
                 if (isRound1) {
                     computer.runFromBeginning();
-                }
-                else {
+                } else {
                     computer.run();
                 }
             });
@@ -322,9 +331,9 @@ const instructionList = readFileToArray('amplifiercircuitinput.txt', ',');
 let maxThrustOutput = 0;
 let bestPhaseSettings = [];
 
-generatePermutations([5,6,7,8,9]).forEach(phaseSettings => {
-    let amplifierCircuit = new AmplifierCircuit(phaseSettings, instructionList);
-    let thrustOutput = amplifierCircuit.run(0);
+generatePermutations([5, 6, 7, 8, 9]).forEach((phaseSettings) => {
+    const amplifierCircuit = new AmplifierCircuit(phaseSettings, instructionList);
+    const thrustOutput = amplifierCircuit.run(0);
 
     if (thrustOutput > maxThrustOutput) {
         maxThrustOutput = thrustOutput;

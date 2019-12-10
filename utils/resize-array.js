7060cfd45ce9a658a3124ec3f41bda90cf4ba14e
Matthew FitzGerald-Chamberlain <chamberlain2007@gmail.com>
2019-12-09 22:20:48 -0600

    Cleanup linting errors in resizeArray

--- utils/resize-array.js
+++ utils/resize-array.js
@@ -3,10 +3,10 @@
  * @param {any[]} arr The array to resize
  * @param {number} newSize The new size
  * @param {any} defaultValue The default value to fill
- * @return The resized array
+ * @return {any[]} The resized array
  */
 const resize = (arr, newSize, defaultValue) => {
-    return [ ...arr, ...Array(Math.max(newSize - arr.length, 0)).fill(defaultValue)];
-}
+    return [...arr, ...Array(Math.max(newSize - arr.length, 0)).fill(defaultValue)];
+};
 
 module.exports = resize;
